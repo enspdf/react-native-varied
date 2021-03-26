7d0ca6ea1e4050e9be251a7f942cc7e13117c791
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2021-03-25 22:15:51 -0500

    Replace reducer logic with zustand implementation

--- ReactNavigationAuthenticationFlowsWithHooks/src/hooks/useAuth.js
+++ ReactNavigationAuthenticationFlowsWithHooks/src/hooks/useAuth.js
@@ -1,82 +1,32 @@
-import {useEffect, useReducer, useMemo} from 'react';
-import axios from 'axios';
+import {useEffect} from 'react';
 import SecureStorage from 'react-native-secure-storage';
 
-import {BASE_URL} from '../config';
-import {createAction} from '../utils/createAction';
 import {sleep} from '../utils/sleep';
+import {useUserStore} from '../stores/userStore';
 
 export function useAuth() {
-  const [state, dispatch] = useReducer(
-    (state, action) => {
-      switch (action.type) {
-        case 'SET_USER':
-          return {
-            ...state,
-            user: {
-              ...action.payload,
-            },
-          };
-        case 'SET_LOADING':
-          return {
-            ...state,
-            loading: action.payload,
-          };
-        case 'REMOVE_USER':
-          return {
-            ...state,
-            user: undefined,
-          };
-        default:
-          return state;
-      }
-    },
-    {user: undefined, loading: true},
-  );
-
-  const auth = useMemo(
-    () => ({
-      login: async (email, password) => {
-        const {data} = await axios.post(`${BASE_URL}/auth/local`, {
-          identifier: email,
-          password,
-        });
-
-        const user = {
-          email: data.user.email,
-          token: data.jwt,
-        };
-
-        await SecureStorage.setItem('user', JSON.stringify(user));
-
-        dispatch(createAction('SET_USER', user));
-      },
-      logout: async () => {
-        await SecureStorage.removeItem('user');
-        dispatch(createAction('REMOVE_USER'));
-      },
-      register: async (email, password) => {
-        await axios.post(`${BASE_URL}/auth/local/register`, {
-          username: email,
-          email,
-          password,
-        });
-      },
-    }),
-    [],
-  );
+  const auth = useUserStore(({login, register, logout}) => ({
+    login,
+    register,
+    logout,
+  }));
+  const state = useUserStore(({loading, user}) => ({loading, user}));
+  const [setUser, setLoading] = useUserStore(state => [
+    state.setUser,
+    state.setLoading,
+  ]);
 
   useEffect(() => {
     sleep(2000).then(() => {
       SecureStorage.getItem('user').then(user => {
         if (user) {
-          dispatch(createAction('SET_USER', JSON.parse(user)));
+          setUser(JSON.parse(user));
         }
 
-        dispatch(createAction('SET_LOADING', false));
+        setLoading(false);
       });
     });
-  }, []);
+  }, [setLoading, setUser]);
 
   return {
     auth,
